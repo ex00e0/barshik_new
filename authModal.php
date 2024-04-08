3ab2f47b576132fd0888aba3502f0935a4f21605
ex00e0 <hasanova.leya@yandex.ru>
2024-04-08 17:43:31 +0500

    работа с модальными окнами

---
+++ authModal.php
@@ -0,0 +1,27 @@
+<div class='modalShadow'></div>
+<div id='modalAuth'>
+    <svg id='authClose' viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.89355 1.57422L13.8786 13.5593" stroke="black" stroke-width="1.91761" stroke-linecap="round"/>
+        <path d="M13.8789 1.57422L1.89381 13.5593" stroke="black" stroke-width="1.91761" stroke-linecap="round"/>
+    </svg>
+    <div class='authHeadline'>Вход</div>
+    <form class='authForm' action='' method='post'>
+        <input type="text" name='email'>
+        <input type="text" name='password'>
+        <input type='submit' value='Войти' class='authButton'>
+    </form>
+    <div class='authSwitch'>Еще нет аккаунта? <span id='authA'> Создать аккаунт</span></div>
+</div>
+<div id='modalReg'>
+    <svg id='regClose' viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.89355 1.57422L13.8786 13.5593" stroke="black" stroke-width="1.91761" stroke-linecap="round"/>
+        <path d="M13.8789 1.57422L1.89381 13.5593" stroke="black" stroke-width="1.91761" stroke-linecap="round"/>
+    </svg>
+    <div class='authHeadline'>Регистрация</div>
+    <form class='authForm' action='' method='post'>
+        <input type="text" name='email'>
+        <input type="text" name='password'>
+        <input type='submit' value='Войти' class='authButton'>
+    </form>
+    <div class='authSwitch'>Есть аккаунт? <span id='regA'> Войти в аккаунт</span></div>
+</div>
---
+++ js/scriptCat.js
@@ -0,0 +1,31 @@
+//модальное окно 1 товара
+let modalClose = document.getElementsByClassName("modalClose")[0];
+let cardCatR3C2 = document.getElementsByClassName("cardCatR3C2");
+let modalShadow = document.getElementsByClassName("modalShadow")[1];
+let modal = document.getElementsByClassName("modal")[0];
+
+modalClose.addEventListener("click", function () {modalShadow.style.display='none';
+                                                modal.style.display='none';} );
+for (let i=0;i<cardCatR3C2.length;i++) {cardCatR3C2[i].addEventListener("click", function () {modalShadow.style.display='block';
+                                                                                            modal.style.display='grid';} );}
+
+//для авторизации
+let modalShadowAuth = document.getElementsByClassName("modalShadow")[0];
+let fromAuthToReg = document.getElementById("authA");
+let fromRegToAuth = document.getElementById("regA");
+let modalAuth = document.getElementById("modalAuth");
+let modalReg = document.getElementById("modalReg");
+let regClose = document.getElementById("regClose");
+let authClose = document.getElementById("authClose");
+let exitEnterButton = document.getElementById('exitEnterButton');
+
+exitEnterButton.addEventListener('click', function () {modalAuth.style.display='grid';
+                                                        modalShadowAuth.style.display='block';} );
+fromAuthToReg.addEventListener('click', function () {modalAuth.style.display='none';
+                                                     modalReg.style.display='grid';} );
+fromRegToAuth.addEventListener('click', function () {modalReg.style.display='none';
+                                                     modalAuth.style.display='grid';} );
+regClose.addEventListener('click', function () {modalReg.style.display='none';
+                                                modalShadowAuth.style.display='none';} );  
+authClose.addEventListener('click', function () {modalAuth.style.display='none';
+                                                modalShadowAuth.style.display='none';} );                                      
